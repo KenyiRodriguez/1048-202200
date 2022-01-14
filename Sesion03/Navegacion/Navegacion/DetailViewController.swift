a380c1932705519bcbd685bcc288039bf989a907
Kenyi Rodriguez <kenyi.rodriguez@globant.com>
2022-01-14 09:16:40 -0500

    Sesion 03

---
+++ Sesion03/Navegacion/Navegacion/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  Navegacion
+//
+//  Created by Kenyi Rodriguez on 14/01/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBAction func clickBtnClose(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
