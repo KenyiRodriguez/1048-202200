5c5456e10fc0b034b701c6a5c5cee1b46c61b8ed
Kenyi Rodriguez <kenyi.rodriguez@globant.com>
2022-01-24 09:20:44 -0500

    Sesion 06

---
+++ Sesion06/Cines/Cines/General/AlertManager.swift
@@ -0,0 +1,34 @@
+//
+//  AlertManager.swift
+//  Cines
+//
+//  Created by Kenyi Rodriguez on 24/01/22.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func showAlertWithTitle(_ title: String, message: String, acceptTitle: String) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let acceptAction = UIAlertAction(title: acceptTitle, style: .cancel, handler: nil)
+        alertController.addAction(acceptAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func showAutomaticAlertWithTitle(_ title: String, message: String) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        self.present(alertController, animated: true) {
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                alertController.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+}
+
